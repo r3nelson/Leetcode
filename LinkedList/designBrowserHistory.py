d20249d35ad06621c1ed2293de15f8b439547526
r3nelson <r3nelson@outlook.com>
2024-03-19 22:34:21 -1000

    added faster implementation using stack to solve BrowserHistory

--- LinkedList/designBrowserHistory.py
+++ LinkedList/designBrowserHistory.py
@@ -11,6 +11,7 @@ class ListNode:
         self.backHistory = leftNode
         self.forwardHistory = rightNode
 
+# Doubly Linked List slower than stack for this problem
 class BrowserHistory:
 
     def __init__(self, homepage: str):
@@ -45,6 +46,36 @@ class BrowserHistory:
         self.currentHistory = cur
 
         return cur.url
+
+
+# faster version using Stack
+class StackImplementationBrowserHistory:
+
+    def __init__(self,homepage:str) -> None:
+        self.index = 0
+        self.len = 1
+        self.history = [homepage]
+
+    def visit(self,url:str) -> None:
+        # if history array isn't long enough just append value
+        if len(self.history) < self.index+2:
+            self.history.append(url)
+        # if history array is long enough change value at self.index + 1
+        else:
+            self.history[self.index + 1] =url
+        # change current index to new value and change length to self.index + 1 to soft delete the forwardHistory values
+        self.index += 1
+        self.len = self.index + 1
+
+    def back(self,steps:int) -> str:
+        self.index = max(self.index - steps, 0)
+        return self.history[self.index]
+    
+    def forward(self,steps:int) -> str:
+        self.index = min(self.index + steps, self.len -1)
+        return self.history[self.index]
+    
+    
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
